90d4c04921e8c03a5115be51c1f953b5bba8ace3
lwj <jg15575446613@163.com>
2025-02-17 20:12:33 +0800

    feat: map函数拆解学习

--- src/eazy/map.js
+++ src/eazy/map.js
@@ -1,40 +1,58 @@
-function map(value, iterateFn) {
-  const ret = []
+// function baseEach(collection, iterateFn) {
+//   if(Array.isArray(collection)) {
+//     for(let i = 0; i <= collection.length - 1; i++) {
+//       const v = collection[i]
 
-  if(Array.isArray(value)) {
-    for(let i = 0; i <= value.length - 1; i++) {
-      const v = value[i]
+//       iterateFn(v, i, collection)
+//     }
+//   } else {
+//     for(let key in  collection) {
+//       const v = collection[key]
+//       iterateFn(v, key, collection)
+//     }
+//   }
+// }
 
-      if(typeof iterateFn === 'function') {
-        ret.push(iterateFn(v, i, value)) 
-      } else {
-        ret.push(v[iterateFn])
-      }
+// function map(value, fnOrKey) {
+//   const ret = []
+//   baseEach(value, (v, k, collection)=> {
+//     if(typeof fnOrKey === 'function') {
+//       ret.push(fnOrKey(v,k,collection))
+//     } else {
+//       ret.push(v[fnOrKey])
+//     }
+//   })
 
-    }
-  } else {
-    for(let key in  value) {
-      const v = value[key]
+//   return ret
+// }
 
-      if(typeof iterateFn === 'function') {
-        ret.push(iterateFn(v, key, value))
-      } else {
-        ret.push(v[iterateFn])
-      }
-    }
+function arrayMap(array, iteratee) {
+  const result = []
+  const length = array == null ? 0 : array.length
+  let i = -1
+  while(++i < length) {
+    result[i] = iteratee(array[i], i, array)
   }
 
-  return ret
-
-  // return Object.entries(value)
-  //   .map(([k, v]) => {
-  //     if(typeof iterateFn === 'function') {
-  //       return iterateFn(v, k, value)
-  //     }
-
-  //     return v[iterateFn]
-  //   })
+  return result
 }
+
+// arrayMap(null, (i)=> console.log(i))
+
+function baseMap(collection, iteratee) {
+  const ret = []
+  baseEach(collection, (value, key, collection)=> {
+    ret.push(iteratee(value, key, collection))
+  })
+
+  return ret
+}
+
+// lodash
+// function map(collection, iteratee) {
+//   const func = isArray(collection) ? arrayMap : baseMap;
+//   return func(collection, getIteratee(iteratee, 3));
+// }
 
 
 function square(n) {
@@ -43,11 +61,11 @@ function square(n) {
 
 // map([4,8], square)
 // => [16,64]
-console.log("🚀 ~ map([4,8], square):", map([4, 8], square))
+// console.log("🚀 ~ map([4,8], square):", map([4, 8], square))
 
 // map({a: 4, b: 8}, square)
 // => [16,64]
-console.log("🚀 ~ map({a: 4, b: 8}, square):", map({ a: 4, b: 8 }, square))
+// console.log("🚀 ~ map({a: 4, b: 8}, square):", map({ a: 4, b: 8 }, square))
 
 
 var users = [
@@ -57,4 +75,9 @@ var users = [
 
 // map(users, 'user')
 // => ['barney', 'fred']
-console.log("🚀 ~ map(users, 'user'):", map(users, 'user'))
+// console.log("🚀 ~ map(users, 'user'):", map(users, 'user'))
+
+// map([], x=> x)
+
+// console.log('🚀 ~ map([], x=> x):', map([], x=> x))
+
